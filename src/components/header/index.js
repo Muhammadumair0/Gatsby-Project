a3012bab07c7dc047dc452156fcb2d43ce20c14c
Muhammadumair0 <muhammadumairkhanyz@gmail.com>
2018-02-25 23:26:09 +0500

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,35 +1,30 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
 
 import './_header.scss';
 
-const Header = () => (
-  <div class="header"
-    style={{
-      background: 'orange',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Groceri Star
-        </Link>
-      </h1>
+const Header = () =>
+  (<header className="header">
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="header-content">
+            <span className="header-logo">
+              Gatsby React Boilerplate
+            </span>
+            <nav className="header-nav">
+              <ul className="header-nav-list">
+                <li className="header-nav-list-item">
+                  <a href="#features">Features</a>
+                </li>
+                <li className="header-nav-list-item">
+                  <a href="#howto">How To</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-)
+  </header>);
 
-export default Header
+export default Header;
